9f302c3d912c1189efeeb7f8396e94589fe88a80
Diana <menestrelia199702@gmail.com>
2023-06-27 14:38:58 +0400

    выносит форму входа в отдельный модуль

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
     //  * Перенести всю разметку в рендер функцию
     //  * Сделать форму входа динамической
     //  * Отрефакторить риложение на модули
+    //  * api (+)
+    //  * вытащить логин компонент в отдельный модуль (+)
+    //  * вытащить компонент списка задач и форму добавления в отедльный модуль
     // 2. Реализовать форму регистрации
 
 import { addTodo, deleteTodo, getTodos } from "./api.js";
+import {renderLoginComponent} from "./components/login-component.js"
 
     // TODO: Получать из хранилища данных
     let tasks = [];
-
-    // let login = prompt("Логин");
 
     let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
     token = null;
@@ -27,27 +29,13 @@ import { addTodo, deleteTodo, getTodos } from "./api.js";
       const appEl = document.getElementById("app");
 
       if (!token) {
-        const appHtml = `
-                <h1>Список задач</h1>
-                <div class="form">
-                <h3 class="form-title">Форма входа</h3>
-                <div class="form-row">
-                    Логин
-                    <input type="text" id="login-input" class="input" />
-                    <br />
-                    Пароль
-                    <input type="text" id="login-input" class="input" />
-                </div>
-                <br />
-                <button class="button" id="login-button">Войти</button>
-                </div>`;
-
-                appEl.innerHTML = appHtml;
-
-                document.getElementById('login-button').addEventListener ("click", () => {
-                     token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-                        fetchTodosAndRender();
-                } )
+                renderLoginComponent({
+                    appEl,
+                    setToken: (newToken) => {
+                    token = newToken;
+                },
+                fetchTodosAndRender
+            });
 
                 return;
       }
@@ -142,6 +130,4 @@ import { addTodo, deleteTodo, getTodos } from "./api.js";
           });
       });
     };
-
-    //fetchTodosAndRender();
     renderApp();
